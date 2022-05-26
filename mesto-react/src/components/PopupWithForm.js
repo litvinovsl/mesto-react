948998a5f905058fe74b820bbd2e5a3992ad8625
Sergey Litvinov <litvinows9@gmail.com>
2022-05-26 23:40:47 +0300

    cleaned comments

--- mesto-react/src/components/PopupWithForm.js
+++ mesto-react/src/components/PopupWithForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import closeIcon from '../images/Close-Icon.svg';
 
-// Функциональный компонент User
 function PopupWithForm(props) {
 
     return (
@@ -22,29 +21,5 @@ function PopupWithForm(props) {
         </section>
     );
 }
-// function PopupWithForm() {
-//   return (
-//     <section id="popup-user" className="popup">
-//         <div className="popup__container">
-//           <button id="popup-user-close" type="button" className="popup__button-close"><img className="popup__button-close-img"
-//             src={closeIcon} alt="Закрыть" />
-//           </button>
-//           <h2 className="popup__edit-text">Редактировать профиль</h2>
-//           <form id="form-profile" className="popup__form" name="profileForm" action="#" novalidate>
-//             <fieldset className="popup__inputs">
-//               <input id="popup__username" type="text" value="Жак-Ив Кусто" className="popup__input" name="name"
-//                 placeholder="Имя" required minlength="2" maxlength="40" />
-//               <span className="popup__username-error popup__input-error"></span>
-//               <input id="popup__user-about" type="text" value="Исследователь океана" className="popup__input"
-//                 name="about" placeholder="О себе.." required minlength="2" maxlength="40" />
-//               <span className="popup__user-about-error popup__input-error"></span>
-//               <button className="popup__button-save" type="submit">Сохранить</button>
-//             </fieldset>
-//           </form>
-//         </div>
-//         <div id="overlayProfile" className="popup__overlay"></div>
-//       </section>
-//   );
-// }
 
 export default PopupWithForm;
