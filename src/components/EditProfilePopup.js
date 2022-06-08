e9773cdd58ce9296b7170f102572dd78601a6c0d
Sergey Litvinov <litvinows9@gmail.com>
2022-06-08 22:40:42 +0300

    changing profile information locally

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -27,6 +27,8 @@ function EditProfilePopup(props) {
             name: name,
             about: description,
         });
+        currentUser.name = name;
+        currentUser.about = description;
     }
 
     return (
